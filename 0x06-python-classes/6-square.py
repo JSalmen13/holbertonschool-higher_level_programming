8021ab877587be51a66f633dc8eb065a9c88e451
Jsalmen13 <Jarraya.salmen13@gmail.com>
2020-09-23 00:34:14 +0100

    add

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -6,7 +6,7 @@
 class Square:
     """square size
     """
-        def __init__(self, size=0, position=()):
+    def __init__(self, size=0, position=()):
         if isinstance(size, int):
             if size < 0:
                 raise ValueError("size must be >= 0")

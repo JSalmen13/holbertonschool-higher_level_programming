7966e89e325c67419a582ebbb2734ee5676e816d
Jsalmen13 <jarraya.salmen13@gmail.com>
2021-02-14 14:41:52 +0000

    same class

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+adds new function
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    returs a boolean based on similarity between two classes
+    """
+    return (type(obj) is a_class)

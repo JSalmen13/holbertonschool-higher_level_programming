4a7d6c5fb11ad62b86038b329c975179c4d25727
Jsalmen13 <jarraya.salmen13@gmail.com>
2021-02-14 14:21:54 +0000

    1st task

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+add new class that inherist list
+"""
+
+
+class MyList(list):
+	"""
+	a new class from list
+	"""
+	def print_sorted(self):
+		"""
+		print sorted list
+		"""
+		print(sorted(self))
